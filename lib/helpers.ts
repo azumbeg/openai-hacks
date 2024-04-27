1e7bda9f25c3a367b0abb8e2b85cc25dc39e9e0b
Azum Beg <azumbeg@gmail.com>
2024-04-27 13:21:52 -0700

    x

---
+++ lib/helpers.ts
@@ -0,0 +1,97 @@
+import axios from "axios";
+
+export interface Data {
+  _id: string;
+  "Created By"?: string;
+  "Created Date": string;
+  "Modified Date": string;
+}
+
+export interface Agent extends Data {
+  agent_name_text: string;
+  cal_api_key_text: string;
+  cal_event_id_number: number;
+  company_name_text: string;
+  owner_user: string;
+  phone_number_text: string;
+  agent_id_text: string;
+}
+
+type ConstraintType =
+  | "equals"
+  | "not equal"
+  | "is_empty"
+  | "is_not_empty"
+  | "text contains"
+  | "not text contains"
+  | "greater than"
+  | "less than"
+  | "in"
+  | "not in"
+  | "contains"
+  | "not contains"
+  | "empty"
+  | "not empty"
+  | "geographic_search";
+
+export const baseUrl = () => {
+  if (process.env.NODE_ENV === "production") {
+    return "https://usemeli.com/api/1.1/obj";
+  }
+
+  return "https://usemeli.com/version-test/api/1.1/obj";
+};
+
+export type Constraint<T> = keyof T extends infer K
+  ? K extends keyof T
+    ? { key: K; constraint_type: ConstraintType; value?: T[K] | T[K][] }
+    : never
+  : never;
+
+export const getItems = async <T>(
+  dataType: string,
+  constraints?: Constraint<T>[]
+): Promise<T[]> => {
+  const items: T[] = [];
+
+  const fetchItems = async (
+    dataType: string,
+    cursor: number,
+    constraints?: Constraint<T>[]
+  ): Promise<any> => {
+    const url = `${baseUrl()}/${dataType}`;
+
+    const params = {
+      cursor,
+      api_token: process.env.BUBBLE_API_KEY,
+      constraints: JSON.stringify(constraints) || null,
+    };
+
+    const { data } = await axios.get(url, { params });
+
+    return data.response;
+  };
+
+  let cursor = 0;
+
+  // Get first items
+  const response = await fetchItems(dataType, cursor, constraints);
+  items.push(...response.results);
+
+  const totalItemCount = response.count + response.remaining;
+  cursor += 100;
+
+  const promises: Promise<any>[] = [];
+  // Get remaining items and push promises to array and await them all
+  while (cursor < totalItemCount) {
+    promises.push(fetchItems(dataType, cursor, constraints));
+    cursor += 100;
+  }
+
+  const results = await Promise.all(promises);
+  for (const result of results) {
+    items.push(...result.results);
+  }
+
+  return items;
+};
--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,13 +1,119 @@
+import OpenAI from "openai";
 import { NextRequest } from "next/server";
+import { z } from "zod";
+
+import { Agent, getItems } from "../../../../lib/helpers";
+import { ChatCompletionTool } from "openai/resources/index.mjs";
+
+const getAgentFromDb = async (agentId: string): Promise<Agent> => {
+  const [agent] = await getItems<Agent>("agent", [
+    { key: "agent_id_text", constraint_type: "equals", value: agentId },
+  ]);
+
+  return agent;
+};
+
+const createLeadInDb = async () => {};
+
+const createLeadSchema: ChatCompletionTool = {
+  type: "function",
+  function: {
+    name: "create_lead",
+    description: "Generate a new lead with the provided information",
+    parameters: {
+      type: "object",
+      properties: {
+        square_footage: {
+          type: "number",
+          description: "The square footage of the property",
+        },
+        service_type: {
+          type: "string",
+          description: "The type of painting service requested",
+          enum: ["interior", "exterior"],
+        },
+        address: {
+          type: "string",
+          description: "The address of the property",
+        },
+        appointment_date: {
+          type: "string",
+          description: "The date of the appointment",
+        },
+      },
+      required: [
+        "square_footage",
+        "service_type",
+        "address",
+        "appointment_date",
+      ],
+    },
+  },
+};
 
 const handler = async (request: NextRequest) => {
   const body = await request.json();
 
   if (body.event === "call_ended") {
-    console.log(body);
+    const { agent_id, transcript } = body.data;
+
+    // Get agent from DB
+    const dbAgent = await getAgentFromDb(agent_id);
+
+    // Extract structured data from phone call using transcript and OpenAI prompt
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY || "",
+      organization: process.env.OPENAI_ORG_ID || "",
+    });
+
+    const prompt = `
+      You will be provided with a transcript of the phone call between customer and an agent at a painting company.
+      The customer is booking an appointment for a painting service and requesting a quote.
+      Your job is to extract the following information from the transcript:
+      - Square footage of the property
+      - Type of painting service requested (interior or exterior)
+      - Address of the property
+      - Appointment date
+    `;
+
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: prompt },
+        { role: "user", content: transcript },
+      ],
+      model: "gpt-4-0125-preview",
+      tools: [createLeadSchema],
+      tool_choice: {
+        type: "function",
+        function: { name: "create_lead" },
+      },
+    });
+
+    const LeadSchema = z.object({
+      square_footage: z.number(),
+      service_type: z.string(),
+      address: z.string(),
+      appointment_date: z.string(),
+    });
+
+    let response;
+    if (chatCompletion.choices[0].message.tool_calls?.[0].function.arguments) {
+      response = JSON.parse(
+        chatCompletion.choices[0].message.tool_calls[0].function.arguments
+      );
+    }
+
+    const leadResult = LeadSchema.safeParse(response);
+
+    console.log({ leadResult });
+
+    // Create lead in DB
   }
 };
 
 export async function POST(request: NextRequest) {
   return handler(request);
 }
+
+// process.env.BUBBLE_API_KEY = "03fa7a4f44e6cd09789e1d2a5882622b";
+// getAgentFromDb("7191d2d48b0311ac95b259935b009ff0");
