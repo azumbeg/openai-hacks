2f60012d8f8a1d749039701531b12a56b9b138ab
Azum Beg <azumbeg@gmail.com>
2024-04-27 14:48:01 -0700

    x

--- src/app/api/retell/route.ts
+++ src/app/api/retell/route.ts
@@ -36,26 +36,43 @@ const handler = async (request: NextRequest) => {
     apiKey: process.env.RETELL_API_KEY || "",
   });
 
+  const generalPrompt = `
+      You are an quoting and booking assistant named ${agent_name}, you work for ${company_name}. 
+      Your objective is to schedule an initial consultation and provide a quote for painting services.
+      You are on a call with the buyer, who is interested in painting services.
+      Communicate concisely and conversationally. Aim for responses in short, clear prose, ideally under 10 words.
+      This succinct approach helps in maintaining clarity and focus during buyer interactions.
+      Your approach should be empathetic and understanding, balancing compassion with maintaining a professional stance on what is best for the buyer.
+      It's important to listen actively and empathize without overly agreeing with the buyer, ensuring that your professional opinion guides the quoting and scheduling process.
+
+  `;
+
   const llm: LlmResponse = await retellClient.llm.create({
-    general_prompt: `Your name is ${agent_name} and you are a quoting and booking assistant for a painting company named ${company_name}. Your objective is to schedule an initial consultation and provice a quote for painting services. Communicate concisely and conversationally. Aim for responses in short, clear prose, ideally under 10 words. This succinct approach helps in maintaining clarity and focus during buyer interactions. Your approach should be empathetic and understanding, balancing compassion with maintaining a professional stance on what is best for the buyer. It's important to listen actively and empathize without overly agreeing with the buyer, ensuring that your professional opinion guides the procurement process.`,
+    general_prompt: generalPrompt,
     begin_message: `Hi, I'm ${agent_name} and I'm an assistant with ${company_name}. How can I help you today?`,
-    starting_state: "determine_service",
-    general_tools: [
-      {
-        type: "end_call",
-        name: "end_call",
-        description:
-          "Hang up the call, triggered only after appointment successfully scheduled.",
-      },
-    ],
+    starting_state: "get_name",
+    general_tools: [],
     states: [
+      {
+        name: "get_name",
+        state_prompt:
+          "You will ask the buyer for their name. Once the buyer has provided their name, transition to determine_service.",
+        edges: [
+          {
+            destination_state_name: "determine_service",
+            description: "Transition to determine the service to be provided.",
+          },
+        ],
+        tools: [],
+      },
       {
         name: "determine_service",
         state_prompt:
-          "You will determine whether the caller is interested in interior or exterior painting services. Once the founder replies, tell them that you'd be happy to give them a quote and book it for them. Once the user has provided their service type, transition to collect_square_footage_and_room_count.",
+          "You will determine whether the caller is interested in interior or exterior painting services. Once the buyer replies, tell them that you'd be happy to give them a quote and book it for them. Once the user has provided their service type, transition to collect_square_footage_and_room_count.",
         edges: [
           {
-            destination_state_name: "collect_square_footage_and_room_count",
+            destination_state_name:
+              "collect_square_footage_and_room_count_and_address",
             description:
               "Transition to collect the room count and square footage of the walls to be painted.",
           },
@@ -63,14 +80,27 @@ const handler = async (request: NextRequest) => {
         tools: [],
       },
       {
-        name: "collect_square_footage_and_room_count",
+        name: "collect_square_footage_and_room_count_and_address",
         state_prompt:
-          "You will collect the number of rooms and the total square footage of the walls to be painted. After the user has provided their room count and square footage, transition to appointment_booking.",
+          "You will collect the address of the property, number of rooms to be painted, and the total square footage of the walls to be painted. After the user has provided their address, room count, and square footage, transition to provide_quote.",
+        edges: [
+          {
+            destination_state_name: "provide_quote",
+            description:
+              "Transition to provide a quote when the address, number of rooms, and total square footage has been collected.",
+          },
+        ],
+        tools: [],
+      },
+      {
+        name: "provide_quote",
+        state_prompt:
+          "You will provide a quote which is 3 times the square footage amount specified. Do not let the buyer negotiate more than a 10% discount. Once the buyer has confirmed the quote, transition to appointment_booking.",
         edges: [
           {
             destination_state_name: "appointment_booking",
             description:
-              "Transition to book an appointment when square footage of the walls has been collected.",
+              "Transition to book an appointment when the quote has been agreed upon.",
           },
         ],
         tools: [],
@@ -78,8 +108,14 @@ const handler = async (request: NextRequest) => {
       {
         name: "appointment_booking",
         state_prompt:
-          "You will book an appointment for an initial consultation with the client.",
-        edges: [],
+          "You will book an appointment for an initial consultation with the client. Please book them for sometime during the next 7 days, within business hours. Once the appointment has been booked, transition to collect_email.",
+        edges: [
+          {
+            destination_state_name: "collect_email",
+            description:
+              "Transition to collect the buyer's email once the appointment has been booked.",
+          },
+        ],
         tools: [
           {
             type: "check_availability_cal",
@@ -95,6 +131,31 @@ const handler = async (request: NextRequest) => {
             description: "Book an appointment for an initial consultation.",
             cal_api_key: cal_api_key,
             event_type_id: cal_event_type_id,
+          },
+        ],
+      },
+      {
+        name: "collect_email",
+        state_prompt:
+          "You will collect the email for the buyer. Once the email has been collected, transition to finish_call.",
+        edges: [
+          {
+            destination_state_name: "finish_call",
+            description:
+              "Transition to collect the buyer's email once the appointment has been booked.",
+          },
+        ],
+      },
+      {
+        name: "finish_call",
+        state_prompt:
+          "You will thank the buyer for their time and tell them that we're excited to work with them. End the call when you think it is appropriate.",
+        tools: [
+          {
+            type: "end_call",
+            name: "end_call",
+            description:
+              "End the call, only when it is appropriate and all information has been collected.",
           },
         ],
       },
