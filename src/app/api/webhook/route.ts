450e16d3bbf72ed8042c4bc0e34cafd87480ba12
Azum Beg <azumbeg@gmail.com>
2024-04-27 12:36:45 -0700

    x

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,6 +1,10 @@
 import { NextRequest } from "next/server";
 
-const handler = async (request: NextRequest) => {};
+const handler = async (request: NextRequest) => {
+  const body = await request.json();
+
+  console.log(body);
+};
 
 export async function POST(request: NextRequest) {
   return handler(request);
